07aba47071e20aebb3314990c0dc2fed2d8fb724
Brenno <ubrennz@gmail.comm>
2026-06-17 15:24:36 -0300

    revisando

--- src/main/java/com/neiloalves/projetospringboot2/config/TestConfig.java
+++ src/main/java/com/neiloalves/projetospringboot2/config/TestConfig.java
@@ -9,15 +9,16 @@ import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
 
-@Configuration // para dizer para o spring q essa classe é de configuração
-@Profile("test") // para dizer para o spring qual vai ser o perfil de teste
+@Configuration
+@Profile("test")
 public class TestConfig implements CommandLineRunner {
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        Usuario u1 = new Usuario(null, "Brenno Pereira", "ubrennz@gmail.com", "988888888", "123456");
         Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         usuarioRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/neiloalves/projetospringboot2/repository/UsuarioRepository.java
+++ src/main/java/com/neiloalves/projetospringboot2/repository/UsuarioRepository.java
@@ -3,6 +3,13 @@ package com.neiloalves.projetospringboot2.repository;
 import com.neiloalves.projetospringboot2.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+// UsuarioRepository vai ser o repository responsavel por fazer operações com a entidade Usuario
+// UsuarioRepository está reutilizando o JPArepository<O tipo da entidade q a pessoa vai acessar, e o tipo da chave sendo Long
 
+// os nossos Repositorys eles serão interfaces, pq o JpaRepository tbm é uma interface
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    // o spring data JPA ele já tem uma implementação padrão para essa interface
+
+    // se a gente definiar aqui o JpaRepository generic, usando a sua entidade e o tipo do id da entidade,
+    // já vamos ter uma implementação padrão para esse tipo especifico q definimos aqui
 }
