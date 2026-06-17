037bcbde6552b562e3f9210d9d677fbe8fc70e26
Brenno <ubrennz@gmail.comm>
2026-06-17 09:26:05 -0300

    Projeto criado

---
+++ src/main/java/com/neiloalves/projetospringboot2/ProjetoSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.neiloalves.projetospringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringBoot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/neiloalves/projetospringboot2/ProjetoSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neiloalves.projetospringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
